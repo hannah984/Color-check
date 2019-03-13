b4d99a4b7510c47ced94d76a127c86c2aa36bac7
hannah984 <43930909+hannah984@users.noreply.github.com>
2019-03-13 15:20:11 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["/scripts"]
+
+PORT = 8000
+httpd = socketserver.TCPServer(("localhost", PORT), handler)
+
+
+httpd.server_name = "myServer"
+httpd.server_port = PORT
+
+print("staring CGI server at http://127.0.0.1:8000")
+
+
+httpd.serve_forever()
